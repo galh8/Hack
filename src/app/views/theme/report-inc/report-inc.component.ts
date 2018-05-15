88448f4af2c290ce950bba3235fd1b06937a8084
ניב באר <blooperz@localhost.localdomain>
2018-05-16 01:40:36 +0300

    web form

--- src/app/views/theme/report-inc/report-inc.component.ts
+++ src/app/views/theme/report-inc/report-inc.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-report-inc',
-  templateUrl: './report-inc.component.html',
-  styleUrls: ['./report-inc.component.scss']
+  selector: "app-report-inc",
+  templateUrl: "./report-inc.component.html",
+  styleUrls: ["./report-inc.component.scss"]
 })
 export class ReportIncComponent implements OnInit {
+  d = new Date(Date.now()).toLocaleString();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   OnReport() {
-    alert('hello');
+    alert("hello");
   }
-
 }
--- src/app/views/theme/update-inc/update-inc.component.ts
+++ src/app/views/theme/update-inc/update-inc.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-update-inc',
-  templateUrl: './update-inc.component.html',
-  styleUrls: ['./update-inc.component.scss']
+  selector: "app-update-inc",
+  templateUrl: "./update-inc.component.html",
+  styleUrls: ["./update-inc.component.scss"]
 })
 export class UpdateIncComponent implements OnInit {
+  d = new Date(Date.now()).toLocaleString();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  OnReport() {
+    alert("hello");
   }
-
 }
