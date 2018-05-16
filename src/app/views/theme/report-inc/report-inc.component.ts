0f63db5f9b17552a54c0f337961c76bde3a8a592
Gal Huta <galhuta@gmail.com>
2018-05-16 13:06:27 +0300

    amir change form

--- src/app/views/theme/report-inc/report-inc.component.ts
+++ src/app/views/theme/report-inc/report-inc.component.ts
@@ -12,8 +12,22 @@ export class ReportIncComponent implements OnInit {
 
   ngOnInit() {}
 
-  OnReport() {
-    alert("hello");
+  OnReport(fname,lname,date, ID, Amount_of_claim, Opened, Estimated,Finished,Guilty,NoGuilty,Type) {
+    var status   = 0;
+    var guilt = false;
+    if(Opened){
+      status=0;
+    }else if(Estimated){
+      status=1;
+    }else if(Finished){
+      status=2;
+    }
+    if(Guilty){
+      guilt = true;
+    }
+    alert('new ALERT!');
+    alert("First Name: " + fname + " Last Name " + lname + " Date: "+date + " Amount: " + Amount_of_claim + " ID: " +ID +" Status: " +status +" Guilty " + guilt +" Type " + Type);
   }
+
   OnSubmit(date, firstName, lastName, ID, ammounOfClaim, Opened, Estimated, Finished, Guilty, Not_guilty, select) { }
 }
